69787f9d03591bf70dde8c827976e8e7675f7b5c
winnie <winnie.trap@gmail.com>
2021-05-20 21:25:02 +0900

    move character by key input

---
+++ Assets/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    public float speed = 25;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        rb2d.AddForce(Vector2.right * speed * x);
+    }
+}
